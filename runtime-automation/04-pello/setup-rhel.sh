13eac2ec83258d0c97be96f2aab08507f3b9e1b0
Matthew Yee <myee@redhat.com>
2025-09-03 10:38:07 -0700

    Add download commands and ownership changes in setup scripts: Included wget commands to download cello and pello tarballs, and updated file ownership to 'rhel' for downloaded files in their respective setup scripts.

--- runtime-automation/04-pello/setup-rhel.sh
+++ runtime-automation/04-pello/setup-rhel.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 echo "Starting module called 04-pello" >> /tmp/progress.log
+
+echo "downloading pello tarball"
+wget -P /root/downloads \
+    https://files.pythonhosted.org/packages/source/P/Pello/Pello-1.0.4.tar.gz
+chown rhel: /root/downloads/Pello-1.0.4.tar.gz
