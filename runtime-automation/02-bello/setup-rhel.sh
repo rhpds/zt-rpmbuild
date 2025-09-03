be6f8c13a8929b8bb52a1eb2273994a694c05c69
Matthew Yee <myee@redhat.com>
2025-09-03 09:40:40 -0700

    Enhance lab environment documentation and update setup script: Added a welcome message to the lab setup guide and included a download command for the bello package in setup-rhel.sh.

--- runtime-automation/02-bello/setup-rhel.sh
+++ runtime-automation/02-bello/setup-rhel.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 echo "Starting module called 02-bello" >> /tmp/progress.log
+
+wget -P /home/rhel/downloads \
+    https://github.com/redhat-developer/rpm-packaging-guide/raw/master/example-code/bello-0.1.tar.gz
+chown rhel: /home/rhel/downloads/bello-0.1.tar.gz
