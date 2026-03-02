8e507c7eee6864d64ace7e91160811deeb10143f
Carl George <carlwgeorge@gmail.com>
2026-03-02 02:40:26 -0600

    Spread out downloading of sources across the relevant modules
    Depending on the size of the concurrent participants, downloading all
    the sources at the beginning of the lab can lead rate-limiting by
    GitHub.  Spread the downloads out to the setup of the module that uses
    them.

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -27,18 +27,5 @@ sed -e '/^metalink=https:\/\/mirrors.fedoraproject.org\/metalink/ s/^/#/' \
 # echo "configuring rhel user bashrc"
 cat /root/.bashrc > /home/rhel/.bashrc
 
-echo "creating rhel downloads directory"
 mkdir -p /home/rhel/downloads
 chown rhel: /home/rhel/downloads
-
-wget -P /home/rhel/downloads \
-    https://github.com/redhat-developer/rpm-packaging-guide/raw/master/example-code/bello-0.1.tar.gz
-
-wget -P/home/rhel/downloads \
-    https://github.com/redhat-developer/rpm-packaging-guide/raw/master/example-code/cello-1.0.tar.gz \
-    https://github.com/redhat-developer/rpm-packaging-guide/raw/master/example-code/cello-output-first-patch.patch
-
-wget -P /home/rhel/downloads \
-    https://files.pythonhosted.org/packages/source/P/Pello/Pello-1.0.4.tar.gz
-
-chown -R rhel: /home/rhel/downloads
